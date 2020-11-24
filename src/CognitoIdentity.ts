2f163b4e5c837aaf5455b3bbb2d6b34b95461edb
cudba <cbarandun@gmail.com>
2020-11-24 11:53:49 +0100

    add signup & login

--- src/CognitoIdentity.ts
+++ src/CognitoIdentity.ts
@@ -1,4 +1,4 @@
-import { CognitoIdentity as AwsCognitoIdentity } from 'aws-sdk';
+import { CognitoIdentity as AwsCognitoIdentity, CognitoIdentityServiceProvider } from 'aws-sdk';
 
 export type IdentityPoolSummary = {
   id: string;
@@ -12,11 +12,112 @@ export type CognitoCredentials = {
   expiration: Date;
 };
 
+export type CognitoAuthentication = {
+  accessToken: string;
+  /**
+   * The expiration period of the authentication result in seconds.
+   */
+  expiresIn: number;
+  tokenType: string;
+  refreshToken: string;
+  idToken: string;
+};
+
+export type CognitoCustomAuthChallenge = {
+  challengeName: string;
+  session: string;
+  challengeParameters: Record<string, any>;
+};
+
+export type CognitoSignUp = {
+  userId: string;
+  confirmed: boolean;
+};
+
 export class CognitoIdentity {
   private readonly cognitoIdentity: AwsCognitoIdentity;
+  private readonly cognitoIdentityServiceProvider: CognitoIdentityServiceProvider;
 
-  constructor(awsCognitoIdentity: AwsCognitoIdentity = new AwsCognitoIdentity()) {
+  constructor(
+    awsCognitoIdentity: AwsCognitoIdentity = new AwsCognitoIdentity(),
+    awsCognitoServiceProvider: CognitoIdentityServiceProvider = new CognitoIdentityServiceProvider()
+  ) {
     this.cognitoIdentity = awsCognitoIdentity;
+    this.cognitoIdentityServiceProvider = awsCognitoServiceProvider;
+  }
+
+  async signUp(clientId: string, username: string, password: string): Promise<CognitoSignUp> {
+    const signUpResult = await this.cognitoIdentityServiceProvider
+      .signUp({
+        ClientId: clientId,
+        Username: username,
+        Password: password,
+      })
+      .promise();
+    return {
+      userId: signUpResult.UserSub,
+      confirmed: signUpResult.UserConfirmed,
+    };
+  }
+
+  async passwordLogin(clientId: string, username: string, password: string): Promise<CognitoAuthentication> {
+    const auth = await this.cognitoIdentityServiceProvider
+      .initiateAuth({
+        ClientId: clientId,
+        AuthFlow: 'USER_PASSWORD_AUTH',
+        AuthParameters: { USERNAME: username, PASSWORD: password },
+      })
+      .promise();
+    if (!auth.AuthenticationResult) {
+      throw new Error(`No authentication received. ChallengeName ${auth.ChallengeName}`);
+    }
+    return {
+      accessToken: auth.AuthenticationResult?.AccessToken!!,
+      expiresIn: auth.AuthenticationResult?.ExpiresIn!!,
+      tokenType: auth.AuthenticationResult?.TokenType!!,
+      idToken: auth.AuthenticationResult?.IdToken!!,
+      refreshToken: auth.AuthenticationResult?.RefreshToken!!,
+    };
+  }
+
+  async initiateCustomAuth(clientId: string, username: string): Promise<CognitoCustomAuthChallenge> {
+    const response = await this.cognitoIdentityServiceProvider
+      .initiateAuth({
+        ClientId: clientId,
+        AuthFlow: 'CUSTOM_AUTH',
+        AuthParameters: { USERNAME: username },
+      })
+      .promise();
+    return {
+      challengeName: response.ChallengeName!!,
+      challengeParameters: response.ChallengeParameters!!,
+      session: response.Session!!,
+    };
+  }
+
+  async sendCustomChallengeResponse(
+    clientId: string,
+    username: string,
+    challengeName: string,
+    session: string,
+    response: string
+  ): Promise<CognitoAuthentication> {
+    const auth = await this.cognitoIdentityServiceProvider
+      .respondToAuthChallenge({
+        ChallengeName: challengeName,
+        ClientId: clientId,
+        ChallengeResponses: { USERNAME: username, ANSWER: response },
+        Session: session,
+      })
+      .promise();
+
+    return {
+      accessToken: auth.AuthenticationResult?.AccessToken!!,
+      expiresIn: auth.AuthenticationResult?.ExpiresIn!!,
+      tokenType: auth.AuthenticationResult?.TokenType!!,
+      idToken: auth.AuthenticationResult?.IdToken!!,
+      refreshToken: auth.AuthenticationResult?.RefreshToken!!,
+    };
   }
 
   async getIdentityId(identityPoolId: string, provider: string, identityToken: string): Promise<string> {
@@ -47,8 +148,8 @@ export class CognitoIdentity {
   }
 
   /*
-    there can be a maximum of 60 identity pools returned
-   */
+      there can be a maximum of 60 identity pools returned
+     */
   async listIdentityPools(maxResults: number = 60): Promise<IdentityPoolSummary[]> {
     const identityPools = await this.cognitoIdentity.listIdentityPools({ MaxResults: maxResults }).promise();
     return (
--- test/CognitoIdentiy.test.ts
+++ test/CognitoIdentiy.test.ts
@@ -1,10 +1,56 @@
 import { CognitoIdentity } from '../src';
 
+const clientId: string = process.env.COGNITO_CLIENT_ID!!;
+const username: string = process.env.COGNITO_USERNAME!!;
+const password: string = process.env.COGNITO_USER_PASSWORD!!;
+
 describe.skip('CognitoIdentity integration tests', () => {
   test('returns a list of all identity pools', async () => {
     const cognitoIdentity = new CognitoIdentity();
     const poolSummaries = await cognitoIdentity.listIdentityPools();
     console.log(poolSummaries);
     expect(poolSummaries.length).toBeGreaterThanOrEqual(1);
+  });
+
+  test('sign up', async () => {
+    const cognitoIdentity = new CognitoIdentity();
+    const result = await cognitoIdentity.signUp(clientId, username, '12345678');
+    console.log(result);
+    expect(result.userId).toBeDefined();
+    expect(result.confirmed).toBeDefined();
+  });
+
+  test('login with password', async () => {
+    const cognitoIdentity = new CognitoIdentity();
+    const credentials = await cognitoIdentity.passwordLogin(clientId, username, password);
+    console.log(credentials);
+    expect(credentials.idToken).toBeDefined();
+    expect(credentials.accessToken).toBeDefined();
+    expect(credentials.refreshToken).toBeDefined();
+    expect(credentials.tokenType).toBeDefined();
+    expect(credentials.expiresIn).toBeDefined();
+  });
+
+  test('initiate custom auth flow', async () => {
+    const cognitoIdentity = new CognitoIdentity();
+    const response = await cognitoIdentity.initiateCustomAuth(clientId, username);
+    expect(response.challengeName).toBeDefined();
+    expect(response.session).toBeDefined();
+  });
+
+  test('send custom response', async () => {
+    const cognitoIdentity = new CognitoIdentity();
+    const credentials = await cognitoIdentity.sendCustomChallengeResponse(
+      clientId,
+      username,
+      'CUSTOM_CHALLENGE',
+      'AYABeP3V6G-Ek4ahQelAlYVno3EAHQABAAdTZXJ2aWNlABBDb2duaXRvVXNlclBvb2xzAAEAB2F3cy1rbXMATmFybjphd3M6a21zOmV1LWNlbnRyYWwtMTo1OTA0OTA4MDk4NTg6a2V5LzRkMmU1YTdmLTFjZDctNDljOS04ZmFjLTJkOWRjYjVjZWY5ZgC4AQIBAHg-pjuNKvPKxtnKU3PADgyxqGsH7MeCPUPkKqeoBvImXgGO0Zg_LhBtcF4zdZBu3r2oAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMHWIVsVTUtTzFklMNAgEQgDuCH_YudX66Mi5Ufh6XmPj3zkSU_w-RQ8q_teELkDArYUFjNhFPdhhwJ2viUUYGcSsFJUQ2LHGj2DhaNgIAAAAADAAAEAAAAAAAAAAAAAAAAAC91_mYVvvQZCHueSTirFrU_____wAAAAEAAAAAAAAAAAAAAAEAAAEnBhW-BvB2lIyAr1nu3RizyZdsEHmZ3Sb9vamiEPew-neWnvu-4seJ0sdzCyWEHymTA5oTMab6nLTcDIVvXemz0DwNbmuXN3KEtO1csc4HQw7fhXJQHmVTBxZCZk2ukQqtrKOqdXo4KMrx9-4E7bf6ibR7MJ8Kh4us2iOwfDVYGFJzJAr-oUecnZ7UspR7v0EIl5W_2jGPuSoginhW7ne1h7xwrrcme-JM9lSJzZhTvVmH7D6U4dmQTdAa6UmrumhJJlOyM_Yoc-jWOKAo6UYOslYGuEmOd9mCgNuqm373Ri4iUqT7kDrQGH9MzShKvCBh0AtHe9EbtE9XJUUuxzKI8KM4MBTX4R_frnoIkLbDUoPL9ZBMvZZSZUC0xf6ql641nhqnLTOtmZW0cTdmAyCEInN6_JGZdM8',
+      '445407'
+    );
+    expect(credentials.idToken).toBeDefined();
+    expect(credentials.accessToken).toBeDefined();
+    expect(credentials.refreshToken).toBeDefined();
+    expect(credentials.tokenType).toBeDefined();
+    expect(credentials.expiresIn).toBeDefined();
   });
 });
